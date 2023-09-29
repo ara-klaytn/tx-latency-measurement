26afecdadae6d26de9af333a9cbaebe64eb315ba
ara-klaytn <palani.aravindh@klaytn.foundation>
2023-09-29 11:19:54 +0800

    Prettier

--- sei-tx-latency-measurement/test.js
+++ sei-tx-latency-measurement/test.js
@@ -1,30 +1,28 @@
-const { generateWallet, restoreWallet, getQueryClient, } = require("@sei-js/core");
+const { generateWallet, restoreWallet, getQueryClient } = require("@sei-js/core");
 
-async function yes(){
+async function yes() {
+  // 12 word mnemonic by default
+  const generatedWallet = await generateWallet();
+  //console.log('generated mnemonic', generatedWallet.mnemonic);
 
-// 12 word mnemonic by default
-const generatedWallet = await generateWallet();
-//console.log('generated mnemonic', generatedWallet.mnemonic);
+  // or restore a wallet given a seed phrase
+  const restoredWallet = await restoreWallet(
+    "seven sample green convince fire utility filter noise venture off stock title"
+  );
 
-// or restore a wallet given a seed phrase
-const restoredWallet = await restoreWallet("seven sample green convince fire utility filter noise venture off stock title");
+  const acc = await restoredWallet.getAccounts();
+  const pubAdd = acc[0].address;
+  console.log(pubAdd);
 
-const acc = await restoredWallet.getAccounts()
-const pubAdd = acc[0].address
-console.log(pubAdd);
-
-
- const queryClient = await getQueryClient("https://rpc.atlantic-2.seinetwork.io/").catch((err) => {
-    console.log(err);
-  return null;
-}
-);
- const balance =  await queryClient.cosmos.bank.v1beta1.allBalances(pubAdd).catch((err) => {
+  const queryClient = await getQueryClient("https://rpc.atlantic-2.seinetwork.io/").catch((err) => {
     console.log(err);
     return null;
-}
-);
-console.log(balance);
+  });
+  const balance = await queryClient.cosmos.bank.v1beta1.allBalances(pubAdd).catch((err) => {
+    console.log(err);
+    return null;
+  });
+  console.log(balance);
 }
 
-yes()
+yes();
